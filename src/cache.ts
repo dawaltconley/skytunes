b18ac558d5631ff4308b5dbfda4a735998a6193e
Dylan Awalt-Conley <web@dylan.ac>
2022-10-16 12:29:16 -0400

    add jsdoc comments to CacheItem

--- src/cache.ts
+++ src/cache.ts
@@ -1,9 +1,20 @@
+/**
+ * An abstraction class to handle items with expensive syncronous calculations.
+ * Its value is only recalculated after it has been manually cleared, or when
+ * any items it depends on have been cleared.
+ */
 class CacheItem<Value = any> {
   #value: Value | null = null
   #recalculate: () => Value
   dependencies: CacheItem[]
   dependents: CacheItem[] = []
 
+  /**
+   * @param calculate - a function that returns a new value whenever a cached
+   * item needs to be recalculated.
+   * @param dependencies - a list of CacheItems that this depends on. this
+   * items cache will be cleared whenever any dependencies are updated.
+   */
   constructor(calculate: () => Value, dependencies: CacheItem[] = []) {
     this.#recalculate = calculate
     this.dependencies = dependencies
@@ -14,16 +25,19 @@ class CacheItem<Value = any> {
     return this.#value !== null
   }
 
+  /** gets the cached value or recalculates it if needed */
   get(): Value {
     return this.#value ?? this.set(this.#recalculate())
   }
 
+  /** sets the cached value and clears its dependents */
   set(v: Value): Value {
     this.clear()
     this.#value = v
     return v
   }
 
+  /** clears the cache of this item and all of its dependents */
   clear() {
     this.dependents.forEach(dep => dep.clear())
     this.#value = null
