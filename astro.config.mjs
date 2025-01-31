d48690e8312f6dce22e53eb2a6014fe5be4790d4
Dylan Awalt-Conley <web@dylan.ac>
2025-01-30 20:13:50 -0500

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://skytunes.app',
+  site: 'https://skytunes.dylan.ac',
   output: 'static',
   vite: {
     css: {
